4e0c3ff1aaa2021306be9e6ba46c70237d7aa703
Rafael Oliveira Franco <rafaeloli200@gmail.com>
2021-08-20 15:29:26 -0300

    Erro

--- src/test/java/Visitor/NaveVisitorTest.java
+++ src/test/java/Visitor/NaveVisitorTest.java
@@ -6,21 +6,21 @@ import static org.junit.jupiter.api.Assertions.*;
 public class NaveVisitorTest {
 
     @Test
-    void exibeAviao() {
+    void mostraAviao() {
         Aviao aviao = new Aviao(1, "Boeing 747");
         NaveVisitor naveVisitor = new NaveVisitor();
         assertEquals("Aviao{id=1, modelo=Boeing 747}", naveVisitor.exibir(aviao));
     }
 
     @Test
-    void exibeHeelicoptero() {
+    void mostraHelicoptero() {
         Helicoptero helicoptero = new Helicoptero(1, "AH-64 Apache", "Galeao");
         NaveVisitor naveVisitor = new NaveVisitor();
         assertEquals("Helicoptero{id=1, modelo=AH-64 Apache, base=Galeao}", naveVisitor.exibir(helicoptero));
     }
 
     @Test
-    void exibeJato() {
+    void mostraJato() {
         Jato jato = new Jato(1, "Bombardier Global", "guarulhos");
         NaveVisitor naveVisitor = new NaveVisitor();
         assertEquals("Jato{id=1, modelo=Bombardier Global, base=guarulhos}", naveVisitor.exibir(jato));
